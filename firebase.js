0d040d7ffd6c80ed8431a1ca1bc7d59b4fc21cee
sangtaek lee <srs99125@naver.com>
2020-08-05 00:02:10 +0900

    Firebase 추가
    1. 기존 BE에서 rest api를 사용하던 것을 firebase firestore로 변경
    2. firebase config를 추가한 firebase.js
    3. html에서 firebase를 사용하기 위한 CDN 추가

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDR5UFZqhtz2QyYofFSemQHBoFQW341CSs",
+    authDomain: "e-commerce-3ee1f.firebaseapp.com",
+    databaseURL: "https://e-commerce-3ee1f.firebaseio.com",
+    projectId: "e-commerce-3ee1f",
+    storageBucket: "e-commerce-3ee1f.appspot.com",
+    messagingSenderId: "313250107871",
+    appId: "1:313250107871:web:d7ba51f71a691f2e8b746a",
+    measurementId: "G-L4S68ELJGT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
