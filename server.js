a4f0cda561571d772937fb5a9e712c3eaba3a3a7
sangtaek lee <srs99125@naver.com>
2020-08-11 23:01:58 +0900

    server.js 생성
    1. npm run start했을 때 index.html을 리턴하는 서버
    2. html, css, js를 한 번에 번들링하는 webpack 적용 필요

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.end(data);
+        }
+    })
+}).listen(8080);
+
+console.log('Server running at http://localhost:8080');
