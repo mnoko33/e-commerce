486ec4bacfc69b8f7e8a0b4d4b6c514be29f3628
sangtaek lee <srs99125@naver.com>
2020-08-13 22:44:55 +0900

    createModal.js 수정
    1. 모달을 열었을 때 전체 화면을 차지하지 않는 문제 해결
    2. 모달을 열었을 때 배경 뒤에서 스크롤이 되는 현상 제거
    3. 모달을 열었을 때 body의 스크롤을 숨김 처리

--- src/utils/createModal.js
+++ src/utils/createModal.js
@@ -16,13 +16,24 @@ export { createModal };
 */
 function createModal(props = {}) {
     const body = document.body;
-    // DEFALUT VALUE
+    // DEFAULT VALUE
     const DEFAULT_VALUE = {
         width: '800px',
         height: '500px',
         confirmBtn: "confirm",
         cancelBtn: "cancel",
-    }
+    };
+
+    // window의 scroll을 발생시키는 events
+    const events = [
+        'keydown',
+        "onwheel" in document ? "wheel" : "mousewheel"
+    ];
+    const scrollHandler = e => {
+        if (e === "onwheel" in document ? "wheel" : "mousewheel" || (e === 'keydown' && (e.which === 38 || e.which === 40))) {
+            e.preventDefault();
+        }
+    };
 
     // modal 본체
     const modal = document.createElement('div');
@@ -30,50 +41,52 @@ function createModal(props = {}) {
     modal.setAttribute("style", `min-width: ${props.width ? props.width : DEFAULT_VALUE.width}; 
                                 min-height: ${props.height ? props.height : DEFAULT_VALUE.height}; 
                                 background-color: #ffffff; 
-                                position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); 
+                                position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); 
                                 z-index: 100000;
-                                border-radius: 10px;`)
+                                border-radius: 10px;`);
     
     const modalHeader = document.createElement('div');
     if (props.title) {
         modalHeader.innerText = props.title;
     }
     // 모달 Header 부분
-    modalHeader.setAttribute("style", "height: 60px; margin: 20px; font-size: 40px; font-weight: bold;")
+    modalHeader.setAttribute("style", "height: 60px; margin: 20px; font-size: 40px; font-weight: bold;");
     const modalBody = document.createElement('div'); 
     const modalFooter = document.createElement('div');
 
     // 모달 Body 부분
-    modalBody.setAttribute("style", "margin: 20px; min-height: calc(100% - 200px);")
-    console.log(props.content);
+    modalBody.setAttribute("style", "margin: 20px; min-height: calc(100% - 200px);");
+
     if (props.content) {
         modalBody.appendChild(props.content);
     }
 
 
     // 모달 Footer 부분
-    modalFooter.setAttribute("style", "height: 80px; margin: 20px 20px 0 20px; display: flex; justify-content: flex-end; border-top: solid black 1px; align-items: center;")
+    modalFooter.setAttribute("style", "height: 80px; margin: 20px 20px 0 20px; display: flex; justify-content: flex-end; border-top: solid black 1px; align-items: center;");
     const removeModal = () => {
         body.removeChild(modal);
         body.removeChild(bg);
-    }
+        body.style.overflow = 'visible';
+        events.forEach(event => window.removeEventListener(event, scrollHandler, {passive: false}));
+    };
     const confirmBtn = document.createElement('button');
     const cancelBtn = document.createElement('button');
-    const btnStyle = "width: 100px; height: 30px; margin: 10px; font-weight: bold;"
+    const btnStyle = "width: 100px; height: 30px; margin: 10px; font-weight: bold;";
 
     // confirm & cancel 버튼
-    confirmBtn.setAttribute("style", btnStyle)
+    confirmBtn.setAttribute("style", btnStyle);
     confirmBtn.innerText = props.btn ? props.btn.confirm : DEFAULT_VALUE.confirmBtn;
     confirmBtn.addEventListener("click", function() {
         if (props.callback) props.callback();
         removeModal()
-    })
+    });
 
-    cancelBtn.setAttribute("style", btnStyle)
+    cancelBtn.setAttribute("style", btnStyle);
     cancelBtn.innerText = props.btn ? props.btn.cancel : DEFAULT_VALUE.cancelBtn;
     cancelBtn.addEventListener("click", function() {
         removeModal()
-    })
+    });
 
     modalFooter.appendChild(cancelBtn);
     modalFooter.appendChild(confirmBtn);
@@ -86,7 +99,7 @@ function createModal(props = {}) {
     // 배경
     const bg = document.createElement('div');
     bg.classNAme = 'bg';
-    bg.setAttribute("style", "background-color: black; opacity: 0.8; width: 100vw; height: 100vh; position: absolute; top: 0; left: 0; z-index: 99999");
+    bg.setAttribute("style", "background-color: black; opacity: 0.8; width: 100vw; height: 100vh; position: fixed; top: 0; left: 0; z-index: 99999");
     bg.addEventListener("click", function() {
         if (props.persistent === undefined || props.persistent === false) {
             removeModal();
@@ -97,10 +110,12 @@ function createModal(props = {}) {
             modal.style.width = `${parseInt(width.slice(0, -2)) + VALUE}px`;
             modal.style.height = `${parseInt(height.slice(0, -2)) + VALUE}px`;
             setTimeout(() => {
-                modal.style.width = width
-                modal.style.height = height
+                modal.style.width = width;
+                modal.style.height = height;
             }, 50)
         }
-    })
+    });
     body.appendChild(bg);
+    body.style.overflow = "hidden";
+    events.forEach(event => window.addEventListener(event, scrollHandler, {passive: false}));
 }
