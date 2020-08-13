69c710424ef48e50d4631615be4a3e12ae1c53c6
sangtaek lee <srs99125@naver.com>
2020-08-13 22:42:34 +0900

    webpack.config 수정
    1. 기존 절대경로 방식을 path.resolve로 변경
    2. ES6를 ES5로 다운그레이드 하기 위해 babel-loader 추가
    3. css을 위한 style-loader 및 css-loader 추가
    4. gif 파일을 위한 file-loader 추가

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: {
-        main: `${__dirname}/../src/main.js`,
+        main: path.resolve(__dirname, '../src/main.js')
     },
     output: {
         filename: 'bundle.js',
-        path: `${__dirname}/../dist`
-    }
-}
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.gif$/,
+                loader: "file-loader",
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../index.html'),
+            filename: './dist.html'
+        })
+    ]
+};
