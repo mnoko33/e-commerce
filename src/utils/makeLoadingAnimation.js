643da8224c1982d476e8b89839ef2d57f02d8c3a
sangtaek lee <srs99125@naver.com>
2020-08-14 14:14:21 +0900

    makeLoadingAnimation.js 수정
    1. element를 선택하는 방법 수정 (document.getElementsByClassName > document.querySelector)

--- src/utils/makeLoadingAnimation.js
+++ src/utils/makeLoadingAnimation.js
@@ -3,7 +3,7 @@ import loadingAnimation from '../../images/loading.gif';
 
 
 function makeLoadingAnimation() {
-    const loadingArea = document.getElementsByClassName('container')[0];
+    const loadingArea = document.querySelector('.container');
     const loadingAnimationGIF = document.createElement('img');
     loadingAnimationGIF.src = loadingAnimation;
     loadingAnimationGIF.setAttribute("style", "width: 800px; display: block; margin: 0px auto;");
