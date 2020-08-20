a68af0c846d2c02c641871c15f784715b4e28089
sangtaek lee <srs99125@naver.com>
2020-08-20 16:23:08 +0900

    불필요한 $마크 제거

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 class Header {
     constructor($app) {
-        this.$header = document.createElement('div');
-        this.$header.className = "header";
-        this.$header.innerHTML = `<h1 class="title">E-COMMERCE</h1>`;
-        $app.appendChild(this.$header);
+        this.header = document.createElement('div');
+        this.header.className = "header";
+        this.header.innerHTML = `<h1 class="title">E-COMMERCE</h1>`;
+        $app.appendChild(this.header);
     }
 }
