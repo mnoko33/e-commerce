5d64298ea5869b3369ceb4ea75429e2da2ef46b3
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-08 00:14:20 +0900

    productInfo.js
    1. bg를 보여줄지 여부를 인라인 스타일로 정하던 것에서 class와 css를 통해 정하는 것으로 수정
    2. 이벤트리스너 추가 부분 함수로 분리
    3. document에 등록된 keydown 리스너가 info 페이지가 닫혔을 때 제거되도록 수정

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -1,61 +1,78 @@
 class ProductInfo {
-    constructor({ $app, product, visible }) {
-        this.bg = document.createElement('div');
-        this.bg.className = 'bg';
-        $app.appendChild(this.bg);
+  constructor({ $app, product, visible }) {
+    this.bg = document.createElement('div');
+    this.bg.className = this.visible ? 'bg on' : 'bg off';
+    $app.appendChild(this.bg);
 
-        this.state = { product, visible };
-        
-        this.render();
-    }
+    this.state = { product, visible };
+    this.keydownEscListener = null;
+    
+    this.render();
+  }
 
-    showProductInfo({ product, visible }) {
-        this.state = { product, visible }
-        this.render();
-    }
+  render() {
+    if (this.state.visible) {
+      const { product } = this.state;
+      this.bg.classList.remove('off');
+      this.bg.classList.add('on');
+      this.bg.innerHTML = `
+        <div class="product-info">
+          <div class="product-info-header">
+            <h1>${product.name}</h1>
+          </div>
+          <button id="close-btn">x</button>
+          <div class="product-info-body">
+            <div class="product-info-img">
+              <img src="${product.imgUrl}" alt="${product.name}" />
+            </div>
+            <div class="product-info-content">
+              <div class="product-info-category">카테고리: ${product.category}</div>
+              <div class="product-info-price">가격: \\${convertWon(product.price)}</div>
+              <div class="product-info-description">${product.description}</div>
+            </div>
+          </div>
+        </div>
+      `;
+      this.addClickEventListener();
+      this.addEscKeydownListener();
 
-    closeProductInfo() {
-        this.state = { ...this.state, visible: false };
-        this.render();
+    } else {
+      this.bg.classList.remove('on');
+      this.bg.classList.add('off');
+      this.bg.innerHTML = '';
     }
+  }
 
-    render() {
-        const product = this.state.product;
-        if (this.state.visible) {
-            this.bg.innerHTML = `
-                <div class="product-info">
-                    <div class="product-info-header">
-                        <h1>${product.name}</h1>
-                    </div>
-                    <button id="close-btn">x</button>
-                    <div class="product-info-body">
-                        <div class="product-info-img">
-                            <img src="${product.imgUrl}" alt="${product.name}" />
-                        </div>
-                        <div class="product-info-content">
-                            <div class="product-info-category">카테고리: ${product.category}</div>
-                            <div class="product-info-price">가격: \\${convertWon(product.price)}</div>
-                            <div class="product-info-description">${product.description}</div>
-                        </div>
-                    </div>
-                </div>
-            `;
-            this.bg.style.display = "block";
-            document.querySelector('#close-btn').addEventListener("click", () => {
-                this.closeProductInfo();
-            }, { once: true });
-            document.querySelector('.bg').addEventListener("click", (e) => {
-                if (e.target.className === 'bg') {
-                    this.closeProductInfo();
-                }
-            });
-            document.addEventListener('keydown', e => {
-                if (e.key === 'Escape') {
-                    this.closeProductInfo();
-                }
-            }, { once: true })
-        } else {
-            this.bg.style.display = "none";
-        }
+  showProductInfo({ product, visible }) {
+    this.state = { product, visible }
+    this.render();
+  }
+
+  closeProductInfo() {
+    this.state.visible = false;
+    this.removeEscKeydownListener();
+    this.render();
+  }
+
+  addClickEventListener() {
+    document.querySelector('.bg').addEventListener('click', e => {
+      if (e.target.id === 'close-btn' || e.target.classList.contains('bg')) {
+        this.closeProductInfo();
+      }
+    })
+  }
+
+  addEscKeydownListener() {
+    this.keydownEscListener = e => {
+      if (e.key === 'Escape') {
+        this.closeProductInfo();
+      }
     }
+    document.addEventListener('keydown', this.keydownEscListener);
+  }
+
+  removeEscKeydownListener() {
+    document.removeEventListener('keydown', this.keydownEscListener);
+    this.keydownEscListener = null;
+  }
 }
