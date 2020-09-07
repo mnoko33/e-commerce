a4aea86b44ea22aad1d428a61ddf924168b96d32
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-08 00:29:19 +0900

    시멘틱 마크업 적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
 class Footer {
-    constructor($app) {
-        this.$app = $app;
-        this.footer = document.createElement('div');
-        this.footer.className = 'footer';
-        this.$app.appendChild(this.footer);
+  constructor($app) {
+    this.$app = $app;
+    this.footer = document.createElement('footer');
+    this.footer.className = 'footer';
+    this.$app.appendChild(this.footer);
 
-        this.render();
-    }
+    this.render();
+  }
 
-    render() {
-        this.footer.innerHTML = `
-            <a target="_blank" href="https://github.com/mnoko33/e-commerce">
-                <img class="github-logo" src="../images/github.png" alt="github">
-            </a>
-            <div>E-COMMERCE PJT</div>
-        `;
-    }
+  render() {
+    this.footer.innerHTML = `
+      <a target="_blank" href="https://github.com/mnoko33/e-commerce">
+        <img class="github-logo" src="../images/github.png" alt="github">
+      </a>
+      <div>E-COMMERCE PJT</div>
+    `;
+  }
 }
