3f0a1c4c82d83203f1c92951a5a1ee9ecf5529f9
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-08 00:16:35 +0900

    App.js
    1. categoryList에 넘기던 불필요한 handleCategoryClick 삭제 (해당 컴포넌트 내부에서 처리할 내용임)
    2. 로딩 화면 보여주고 숨기는 부분을 함수로 분류
    3. api fetch부분을 콜백 함수에 넣지 않고 따로 함수로 분류

--- src/App.js
+++ src/App.js
@@ -1,83 +1,88 @@
 class App {
-    constructor($app) {
-        this.$app = $app;
-        this.data = [];
-        this.loadingVisibility = false;
+  constructor($app) {
+    this.$app = $app;
+    this.data = [];
+    this.loadingVisibility = false;
 
-        this.header = new Header($app);
+    this.header = new Header($app);
 
-        this.categoryList = new CategoryList({ 
-            $app, 
-            handleCategoryClick: e => {
-                const CLASS_NAME = "selected";
-                const targetUpdated = e.target;
-                const targetRemoved = document.querySelector(`.${CLASS_NAME}`);
-                
-                targetUpdated.classList.add(CLASS_NAME);
-                targetRemoved.classList.remove(CLASS_NAME);
-            },
-            updateProductList: (categoryName) => {
-                api.getProducts(categoryName)
-                    .then(newData => this.updateData(newData))
-            }
+    this.categoryList = new CategoryList({ 
+      $app, 
+      updateProductList: (categoryName) => {
+        api.getProducts(categoryName)
+          .then(newData => this.updateData(newData))
+      }
+    });
+
+    this.productList = new ProductList({
+      $app,
+      initialData: this.data,
+      handleProductClick: (product) => {
+        this.productInfo.showProductInfo({ 
+          product, 
+          visible: true 
         });
+        this.sidebar.addRecentlyViewed(product);
+      }
+    })
 
-        this.productList = new ProductList({
-            $app,
-            initialData: this.data,
-            handleProductClick: (product) => {
-                this.productInfo.showProductInfo({ 
-                    product, 
-                    visible: true 
-                });
-                this.sidebar.addRecentlyViewed(product);
-            }
-        })
-
-        this.productInfo = new ProductInfo({ 
-            $app, 
-            product: null, 
-            visible: false, 
-        });
+    this.productInfo = new ProductInfo({ 
+      $app, 
+      product: null, 
+      visible: false, 
+    });
         
-        this.sidebar = new Sidebar({ 
-            $app,
-            handleSidebarClick: () => {
-                this.toggleSidebar();
-            },
-            handleProductClick: (product) => {
-                this.productInfo.showProductInfo({ 
-                    product, 
-                    visible: true,
-                });
-            }
+    this.sidebar = new Sidebar({ 
+      $app,
+      handleSidebarClick: () => {
+          this.toggleSidebar();
+      },
+      handleProductClick: (product) => {
+        this.productInfo.showProductInfo({ 
+          product, 
+          visible: true,
         });
+      }
+    });
 
-        this.footer = new Footer($app);
+    this.footer = new Footer($app);
 
-        this.loading = new Loading({
-            $app,
-            visible: true,
-        });
+    this.loading = new Loading({
+      $app,
+      visible: true,
+    });
 
-        api.getProducts('전체보기').then(newData => {
-            setTimeout(() => {
-                this.updateData(newData)
-                this.loading.toggleVisibility({ visible: false });
-            }, 2500)
-        });
-    }
+    api.getProducts('전체보기').then(newData => {
+      setTimeout(() => {
+        this.updateData(newData)
+        this.loading.toggleVisibility({ visible: false });
+      }, 2500)
+    });
+  }
 
-    updateData(newData) {
-        this.data = newData;
-        this.productList.updateProducts(newData);
-    }
+  updateData(newData) {
+    this.data = newData;
+    this.productList.updateProducts(newData);
+  }
 
-    toggleSidebar() {
-        this.sidebar.toggle();
-    }
+  toggleSidebar() {
+    this.sidebar.toggle();
+  }
 
-    toggleLoadingAnime() {
+  beforeFetchApi() {
+    this.loading.setLoadingOn();
+  }
 
-    }
+  beforeUpdateData() {
+    this.loading.setLoadingOff();
+  }
+
+  fetchGetProductsApi(categoryName) {
+    this.beforeFetchApi();
+    api.getProducts(categoryName)
+      .then(newData => {
+        this.beforeUpdateData();
+        this.updateData(newData);
+      })
+  }
 }
