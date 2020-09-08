64ac17d37280e8a0a16f4d0e3dc79b61849eb9ba
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-09 01:19:11 +0900

    Sidebar.js
    1. 최근 본 상품을 기존 리스트에서 Queue로 변경
    2. toggle 메서드 하나로 사이드바를 컨트롤 하던 것을 openSidebar와 closeSidebar로 나누고 이를 호출하는 toggleSidebar 메서드 추가
    3. queue에 아이템을 넣는 로직을 메서드로 분리

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,79 +1,86 @@
 class Sidebar {
-    constructor({ $app, handleSidebarClick, handleProductClick }) {
-        this.sidebar = document.createElement('div');
-        this.sidebar.className = 'sidebar';
-        $app.appendChild(this.sidebar);
+  MAX_QUEUE_SIZE = 3
+  constructor({ $app, handleProductClick, MAX_QUEUE_SIZE }) {
+    this.sidebar = document.createElement('div');
+    this.sidebar.className = 'sidebar hidden';
+    $app.appendChild(this.sidebar);
 
-        this.handleSidebarClick = handleSidebarClick;
-        this.handleProductClick = handleProductClick;
+    this.handleProductClick = handleProductClick;
 
-        this.state = {
-            list: [],
-            itemLimit: 3,
-            toggle: false,
-        }
+    this.MAX_QUEUE_SIZE = MAX_QUEUE_SIZE;
+    this.list = new recentlyViewedQueue(this.MAX_QUEUE_SIZE);
+    this.visible = false;
 
-        this.render();
+    this.render();
+  }
+
+  openSidebar() {
+    this.visible = true;
+    document.querySelector('.sidebar-toggle-btn').innerText = '▶';
+    ['.sidebar-body', '.sidebar'].forEach(selector => {
+      document.querySelector(selector).classList.remove('hidden');
+    })
+  }
+  
+  closeSidebar() {
+    this.visible = false;
+    document.querySelector('.sidebar-toggle-btn').innerText = '◀';
+    ['.sidebar-body', '.sidebar'].forEach(selector => {
+      document.querySelector(selector).classList.add('hidden');
+    })
+  }
+
+  toggleSidebar() {
+    if (this.visible) {
+      this.closeSidebar();
+    } else {
+      this.openSidebar();
     }
+  }
 
-    toggle = () => {
-        this.state = { ...this.state, toggle: !this.state.toggle }
-        const toggle = this.state.toggle;
-        const sidebarBody = document.querySelector('.sidebar-body')
-        this.sidebar.style.width = toggle ? "180px" : "50px";
-        sidebarBody.style.right = toggle ?  "0px" : "-130px";
-        sidebarBody.style.padding = toggle ? "0px 10px 10px" : "0px";
-        document.querySelector('.sidebar-body').style.right = toggle ?  "0px" : "-130px";
-        document.querySelector('.sidebar-toggle-btn').innerText = toggle ? "▶" : "◀";
-    }
+  // 상품 리스트에서 상품을 클릭하면 해당 상품을 recentlyViewedQueue에 추가
+  addRecentlyViewed = (product) => {
+    this.list.push(product);
+    this.renderRecentlyViewedItems();
+  }
 
-    addRecentlyViewed = (product) => {
-        const list = this.state.list;
-        const idx = list.findIndex(item => product.id === item.id);
-        if (idx > -1) {
-            list.splice(idx, 1);
-        } else if (list.length >= this.state.itemLimit) {
-            list.shift();
-        }
-        list.push(product);
-        document.querySelector('.recently-viewed-wrapper').innerHTML = `
-            ${this.state.list.map(
-                product => `
-                    <div class="recently-viewed-product" id="product-${product.id}">
-                        <img src="${product.imgUrl}" width="110" alt="${product.name}">
-                        <div>${product.name}</div>
-                    </div>
-                `
-                ).join("")
-            }
-        `;
-
-        const recentlyViewedBox = document.querySelector('.sidebar-body');
-
-        recentlyViewedBox.addEventListener("click", (e) => {
-            const clickedProduct = e.target.parentNode
-            if (clickedProduct.className === 'recently-viewed-product') {
-                e.stopPropagation();
-                console.log()
-                const clikedId = clickedProduct.id.split('-')[1];
-                const product = this.state.list.find(elem => elem.id == clikedId)
-                if (product) {
-                    this.handleProductClick(product);
-                }
-            }
-        })
-    }
-
-    render() {
-        const LEFT = '◀';
-        const RIGHT = '▶';
-        this.sidebar.innerHTML = `
-            <div class="sidebar-toggle-btn">${this.state.toggle ? RIGHT : LEFT}</div>
-            <div class="sidebar-body">
-                <div class="recently-viewed">최근 본 상품</div>
-                <div class="recently-viewed-wrapper"></div>
-            </div>
+  // this.list를 화면에 렌더링하고 이벤트 리스너를 추가
+  renderRecentlyViewedItems() {
+    document.querySelector('.recently-viewed-wrapper').innerHTML = `
+      ${this.list.getQ().map(
+        product => `
+          <div class="recently-viewed-product" data-id="${product.id}">
+            <img src="${product.imgUrl}" width="110" alt="${product.name}">
+            <div>${product.name}</div>
+          </div>
         `
-        document.querySelector('.sidebar-toggle-btn').addEventListener("click", this.toggle);
-    }
+        ).join("")
+      }
+    `;
+    
+    const recentlyViewedItems = document.querySelectorAll('.recently-viewed-product')
+    recentlyViewedItems.forEach(item => item.addEventListener('click', e => {
+      const selectedId = e.currentTarget.dataset.id;
+      const product = this.list.getQ().find(item => item.id == selectedId);
+      if (product) {
+        this.handleProductClick(product.id);
+      }
+    }))
+  }
+
+  render() {
+    const LEFT = '◀';
+    const RIGHT = '▶';
+    this.sidebar.innerHTML = `
+      <div class="sidebar-toggle-btn">${this.visible ? RIGHT : LEFT}</div>
+      <div class="sidebar-body hidden">
+        <div class="recently-viewed">최근 본 상품</div>
+        <div class="recently-viewed-wrapper"></div>
+      </div>
+    `
+
+    document.querySelector('.sidebar-toggle-btn').addEventListener("click", () => {
+      this.toggleSidebar();
+    });
+  }
 }
