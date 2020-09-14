abb09ea3abc33a6d8ba9706dccbabd584cd9e535
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-14 15:54:08 +0900

    [수정] api 요청에서 delay를 주는 함수가 중복 사용된 것을 수정

--- src/api.js
+++ src/api.js
@@ -9,35 +9,34 @@ function delay () {
 
 const api = {
   getProducts: async category => {
-      if (dummyMode) {
-        await delay();
-        return new Promise(resolve => {
-          resolve(dummyData.map((product, idx) => {
-            return { ...product, id: idx }
-          }))
-        })
+    await delay();
+    if (dummyMode) {
+      return new Promise(resolve => {
+        resolve(dummyData.map((product, idx) => {
+          return { ...product, id: idx }
+        }))
+      })
+    }
+    return new Promise((resolve) => {
+      if (category === '전체보기') {
+        resolve(db.collection('products').get());
+      } else {
+        resolve(db.collection('products').where("category", "==", category).get());
       }
-      await delay();
-      return new Promise((resolve) => {
-        if (category === '전체보기') {
-          resolve(db.collection('products').get());
-        } else {
-          resolve(db.collection('products').where("category", "==", category).get());
-        }
+    })
+    .then(querySnapshot => {
+      return querySnapshot.docs.map(doc => {
+        const data = doc.data();
+        data.id = doc.id;
+        return data;
       })
-      .then(querySnapshot => {
-        return querySnapshot.docs.map(doc => {
-          const data = doc.data();
-          data.id = doc.id;
-          return data;
-        })
-      })
-      .catch(err => {
-        console.error(err);
-        return dummyData.map((product, idx) => {
-                    return { ...product, id: idx }
-                });
-      })
+    })
+    .catch(err => {
+      console.error(err);
+      return dummyData.map((product, idx) => {
+                  return { ...product, id: idx }
+              });
+    })
   },
 
   getProductById: async pid => {
